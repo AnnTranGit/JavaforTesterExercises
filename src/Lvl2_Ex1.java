39184b2dd2ec662670a6d79889fbd1c7277cba41
AnnTran <tranvinhthuyan9797@gmail.com>
2025-09-28 23:25:44 +0700

    commit

--- src/Lvl2_Ex1.java
+++ src/Lvl2_Ex1.java
@@ -21,6 +21,35 @@ public class Lvl2_Ex1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Nhập thu nhập hàng năm từ người dùng");
+        int income = sc.nextInt();
+
+        double taxRate;
+
+        // 2. Xác định thuế suất theo mức thu nhập
+        if (income <= 5) {
+            taxRate = 0.05;
+        } else if (income <= 10) {
+            taxRate = 0.10;
+        } else if (income <= 18) {
+            taxRate = 0.15;
+        } else if (income <= 32) {
+            taxRate = 0.20;
+        } else if (income <= 52) {
+            taxRate = 0.25;
+        } else if (income <= 80) {
+            taxRate = 0.30;
+        } else {
+            taxRate = 0.35;
+        }
+
+        //tinh thue
+        double taxAmount = income * taxRate;
+
+        System.out.println("Thu nhập: " + income + " triệu");
+        System.out.println("% Thuế: " + (taxRate * 100) + "%");
+        System.out.println("Số thuế phải trả: " + taxAmount + " triệu đồng");
+
 
     }
 }
