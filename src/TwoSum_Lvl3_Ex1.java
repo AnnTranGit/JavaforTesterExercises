42a95f5a7800fe0292c7d45a2e4b90187d0401da
AnnTran <tranvinhthuyan9797@gmail.com>
2025-09-29 19:25:02 +0700

    commit

--- src/TwoSum_Lvl3_Ex1.java
+++ src/TwoSum_Lvl3_Ex1.java
@@ -10,16 +10,16 @@ Output: [1,2]
 import java.util.Arrays;
 
 public class TwoSum_Lvl3_Ex1 {
-    public static int[] twoSum(int[] numArray, int target ) {
+    public static int[] twoSum(int[] numArray, int target) {
 
-        int left = 0 ; //1st index
+        int left = 0; //1st index
         int right = numArray.length - 1; //last index
 
-        while (left < right){
+        while (left < right) {
             int sum = numArray[left] + numArray[right]; //tong sum = giá trị của index right + left
 
-            if (sum == target){
-                return new int[]{left + 1 , right + 1}; // do bắt trả về chỉ số (index) của chúng (bắt đầu từ 1).
+            if (sum == target) {
+                return new int[]{left + 1, right + 1}; // do bắt trả về chỉ số (index) của chúng (bắt đầu từ 1).
             } else if (sum < target) {
                 left++;
             } else { //case sum > target
@@ -33,14 +33,14 @@ public class TwoSum_Lvl3_Ex1 {
     }
 
     public static void main(String[] args) {
-       int[] numArr ={2,7,11,15} ;
-       int target = 9;
-       int[] res = twoSum(numArr,target);
+        int[] numArr = {2, 7, 11, 15};
+        int target = 9;
+        int[] res = twoSum(numArr, target);
         System.out.println(Arrays.toString(res));
 
-        int[] numArr1 ={2,7,11,15} ;
+        int[] numArr1 = {2, 7, 11, 15};
         int target1 = 26;
-        int[] res1 = twoSum(numArr1,target1);
+        int[] res1 = twoSum(numArr1, target1);
         System.out.println(Arrays.toString(res1));
 
     }
